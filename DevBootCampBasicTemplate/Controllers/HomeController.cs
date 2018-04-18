0019586c0fc032610ad2264c6f86e80b315bb8fa
AzureDevBootCamp <38484981+AzureDevBootCamp@users.noreply.github.com>
2018-04-18 05:56:54 -0400

    First Deployment
    First Deployment of bootcamp material

--- DevBootCampBasicTemplate/Controllers/HomeController.cs
+++ DevBootCampBasicTemplate/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +13,115 @@ namespace DevBootCampBasicTemplate.Controllers
 {
     public class HomeController : Controller
     {
+        static HttpClientHandler handler = new HttpClientHandler();
+        static HttpClient client = new HttpClient(handler,false);
+        
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult SlowResponse()
         {
             ViewBag.Message = "Your application description page.";
+            //var product = "value";
+            try
+            {
+                using (var client = new HttpClient(handler, false))
+                {
+                    var response = client.GetAsync("https://jebrook-functionapp2.azurewebsites.net/api/HttpTriggerCSharp3").Result;
+                }
+                System.Threading.Thread.Sleep(5000);
+                ViewBag.Message = "Response from the remote endpoint successful";
+
+                var connStr = $"Server=tcp:devbootcamp.database.windows.net,1433;Initial Catalog=devbootcamp;Persist Security Info=False;User ID=devbootcamp;Password=Pa$$w0rd!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
+
+                using (SqlConnection connection = new SqlConnection(connStr))
+                using (SqlCommand command = new SqlCommand("", connection))
+                {
+                    connection.Open();
+                    ///TEST opening the connection to the web app 
+                    command.CommandText = @"SELECT TOP (1000) [ProductID] ,[Name],[ProductNumber]FROM [SalesLT].[Product]";
+                    command.ExecuteNonQuery();
+                    SqlDataReader reader = command.ExecuteReader();
+                }
+
+        }
+            catch (Exception e)
+            {
+                ViewBag.Message = ("Exception : " + e.ToString());
+            }
+
+            //  }
 
             return View();
         }
-
-        public ActionResult Contact()
+        private static void ExecuteInForeground()
         {
-            ViewBag.Message = "Your contact page.";
+            throw new Exception("this is second chance exception");
+        }
+
+        public ActionResult Exception()    ///crash the application
+        {
+            {
+                var th = new System.Threading.Thread(ExecuteInForeground);
+                th.Start();
+
+            }
 
             return View();
         }
+
+        public ActionResult TLSandSSL()     //TLS errors
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            using (var client = new HttpClient(handler, false))
+            {
+                try
+                {
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+                    var response = client.GetAsync("https://www.wellsfargo.com").Result;
+                    ViewBag.Message = "Response from the remote endpoint : " + response;
+                    client.Dispose();
+                }
+                catch (Exception e)
+                {
+                    ViewBag.Message = ("Exception : " + e.ToString());
+                    return View();
+                }
+            }
+            return View();
+        }
+
+    }
+
+    internal class SaleModel
+    {
+        string productID;
+        string Name;
+        string ProductNumber;
+
+        public SaleModel()
+        {
+            productID = "null" ;
+            Name = "null";
+            ProductNumber = "null";
+        }
+        public SaleModel(string pI, string nm, string PN)
+        {
+            productID = pI;
+            Name = nm;
+            ProductNumber = PN;
+        }
+        public void SetName(string pI, string nm, string PN)
+        {
+            productID = pI;
+            Name = nm;
+            ProductNumber = PN;
+        }
+
     }
 }
