fb55148f8d28462c5c62126f55cc8a629c3960d4
Jeremy Brooks <lilman_2010@hotmail.com>
2018-06-27 20:56:24 -0400

    commit
    commit

--- DevBootCampBasicTemplate/App_Start/RouteConfig.cs
+++ DevBootCampBasicTemplate/App_Start/RouteConfig.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,6 +20,24 @@ namespace DevBootCampBasicTemplate
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
+            //throw exception during route config
+           /* var connStr = $"server";
+            if (ConfigurationManager.ConnectionStrings["myConnString"] == null)
+            {
+                connStr = $"Server=tcp:xxx.database.windows.net,1433;Initial Catalog=xxxx;Persist Security Info=False;User ID=xxx;Password=xxxx;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=5;";
+            }
+            else
+            {
+                connStr = ConfigurationManager.ConnectionStrings["myConnString"].ConnectionString;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connStr))
+            {
+                connection.Open();   ///TEST opening the connection to the web app 
+            }
+            */
         }
     }
 }
--- DevBootCampBasicTemplate/Controllers/HomeController.cs
+++ DevBootCampBasicTemplate/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +13,66 @@ namespace DevBootCampBasicTemplate.Controllers
 {
     public class HomeController : Controller
     {
+        static HttpClient client = new HttpClient();
+
         public ActionResult Index()
         {
+            System.Diagnostics.Trace.TraceError("If you're seeing this, something bad happened");
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult SlowResponse()
         {
             ViewBag.Message = "Your application description page.";
+ 
+
+            using (client)
+            {
+                try
+                {
+                    var response = client.GetAsync("https://jebrook-reverseproxy2.azurewebsites.net/oversize_pdf_test_0.pdf").Result;
+                    System.Threading.Thread.Sleep(30000);
+                    ViewBag.Message = "Response from the remote endpoint : " + response.Content;
+                }
+                catch (Exception e)
+                {
+                    ViewBag.Message = ("Exception : " + e.ToString());
+                }
+
+            }
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Exception()
         {
-            ViewBag.Message = "Your contact page.";
+            //throw an unhandled exception 
+
+            IntPtr ptr = new IntPtr(123);
+            Marshal.StructureToPtr(123, ptr, true);
+
 
             return View();
         }
+
+        public ActionResult TLSandSSL()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            using (client)
+            {
+                try
+                {
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+                    var response = client.GetAsync("https://www.wellsfargo.com").Result;
+                    ViewBag.Message = "Response from the remote endpoint : " + response;
+                }
+                catch (Exception e)
+                {
+                    ViewBag.Message = ("Exception : " + e.ToString());
+                }
+            }
+            return View();
+        }
+
     }
 }
